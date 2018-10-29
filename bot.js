c4172e0e72977d3f6bf8479479ad030fed9a2b03
Support3nih <44061131+Support3nih@users.noreply.github.com>
2018-10-29 16:02:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -616,7 +616,6 @@ if(message.content.startsWith(prefix + 'bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let copy = "FireShop";
 let request = `Requested By ${message.author.username}`;
 if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
 msg.react('✅')
@@ -640,7 +639,7 @@ var bc = new
    .addField('المرسل', message.author.username)
    .addField('الرسالة', args)
    .setThumbnail(message.author.avatarURL)
-   .setFooter(copy, client.user.avatarURL);
+   .setFooter(FireShop, client.user.avatarURL);
 m.send({ embed: bc })
 msg.delete();
 })
